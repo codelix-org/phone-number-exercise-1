12fe0c092b19577f2f16131229aaee58e6ecd18e
Jessica Findley <jessica.findley@simplybusiness.co.uk>
2023-04-28 15:48:36 +0100

    added code to make specs pass

--- lib/formatter/phone_number.rb
+++ lib/formatter/phone_number.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
 module Formatter
   class PhoneNumber
-
+    def self.call(number)
+      number.delete!(" ")
+      if number[0] == "+" || number[0] == "4"
+        number[0] == "+" ? number.slice!(0..2) : number.slice!(0..1)
+        number.insert(0, "0")
+      else
+        number
+      end
+    end
   end
 end
