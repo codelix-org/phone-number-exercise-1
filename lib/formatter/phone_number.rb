f5bd89dbf8ed4fb501d2c0f3dc1236dc07f5dfd5
Jessica Findley <jessica.findley@simplybusiness.co.uk>
2023-06-16 13:12:45 +0100

    improved code

--- lib/formatter/phone_number.rb
+++ lib/formatter/phone_number.rb
@@ -4,12 +4,8 @@ module Formatter
   class PhoneNumber
     def self.call(number)
       number.delete!(" ")
-      if number[0] == "+" || number[0] == "4"
-        number[0] == "+" ? number.slice!(0..2) : number.slice!(0..1)
-        number.insert(0, "0")
-      else
-        number
-      end
+      number = number.gsub("+44", "0")
+      number = number.gsub("44", "0")
     end
   end
 end
